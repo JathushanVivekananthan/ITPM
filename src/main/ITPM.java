3d121740b71a6dfa1f08fc3f8a5607dce13e0f23
JathushanVivekananthan <jathu.js1@gmail.com>
2020-05-22 14:09:47 +0530

    Interfaces

---
+++ src/main/ITPM.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package main;
+
+import gui.Home;
+
+/**
+ *
+ * @author Jathushan
+ */
+public class ITPM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home home = new Home();
+        home.setVisible(true);
+    }
+    
+}
